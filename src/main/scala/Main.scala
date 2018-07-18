919b22a3c3615f3a600b2d70c6ef47b42f6390e0
Maldus512 <mattia512maldini@gmail.com>
2018-07-18 11:31:43 +0200

    refactoring

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,4 +1,4 @@
-//package main.scala.com.maldus.spark
+package main.scala.movierank
 
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
@@ -7,23 +7,10 @@ import org.apache.spark.rdd.RDD
 import scala.collection.Map
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat
 import org.apache.hadoop.io.{LongWritable, Text}
+import movies.Movie
 
-class Movie (ms : Map[String, String]) extends Serializable {
-    val productId : String= ms getOrElse ("productId", "")
-    val userId :String = ms getOrElse ("userId", "")
-    val profileName : String = ms getOrElse ("profileName", "")
-    val utilita :String = ms getOrElse ("helpfulness", "0")
-    val score: Float = ms getOrElse ("score", "0.0") toFloat
-    val time : Long = ms getOrElse ("time", "0") toLong
-    val summary : String = ms getOrElse ("summary", "")
-    val text : String = ms getOrElse("text", "")
-    override def toString = "Product: " + productId +
-     "\n\t summary: \t" + summary + 
-     "\n\t score: \t" + score.toString
 
-}
-
-object ExampleJob {
+object Main {
   def main(args: Array[String]) {
     val path: String = args(0)
     val conf = new SparkConf().setAppName("SparkJoins").setMaster("local")
---
+++ src/main/scala/Movie.scala
@@ -0,0 +1,19 @@
+package movies
+
+import scala.collection.Map
+
+
+class Movie (ms : Map[String, String]) extends Serializable {
+    val productId : String= ms getOrElse ("productId", "")
+    val userId :String = ms getOrElse ("userId", "")
+    val profileName : String = ms getOrElse ("profileName", "")
+    val utilita :String = ms getOrElse ("helpfulness", "0")
+    val score: Float = ms getOrElse ("score", "0.0") toFloat
+    val time : Long = ms getOrElse ("time", "0") toLong
+    val summary : String = ms getOrElse ("summary", "")
+    val text : String = ms getOrElse("text", "")
+    override def toString = "Product: " + productId +
+     "\n\t summary: \t" + summary + 
+     "\n\t score: \t" + score.toString
+
+}
